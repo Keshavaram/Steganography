d4724da317aedaf27b94f0717ce396e490036722
Keshavaram <keshavjayanth1401@gmail.com>
2021-10-17 01:04:07 +0530

    Removed all comments

--- main.py
+++ main.py
@@ -52,9 +52,6 @@ def decryption(n, dycrpt, cipher):
 
 # *********************************************************************************************************************************************************************************************************************************************************************************************
 # steganography part
-
-
-# FUNCTION DEFINITION TO CONVERT A DATA INTO BINARY
 def messageToBinary(message):
     if type(message) == str:
         return ''.join([format(ord(i), "08b") for i in message])
@@ -65,35 +62,24 @@ def messageToBinary(message):
     else:
         raise TypeError("Input type not supported")
 
-
-# FUNCTION DEFINITION TO HIDE SECRET MESSAGE
 def hideData(image, secret_message):
-    # calculate the maximum bytes to encode
     n_bytes = image.shape[0] * image.shape[1] * 3 // 8
-    # print("Maximum bytes to encode:", n_bytes)
-
-    # check if the no of bytes to encode is less than the maximum bytes in the image
     if len(secret_message) > n_bytes:
         raise ValueError("Error encountered insufficient bytes,need bigger image or less data.")
     secret_message += "#####"
     data_index = 0
-
-    # convert input data to binary format using messageToBinary()
     binary_secret_msg = messageToBinary(secret_message)
-    data_len = len(binary_secret_msg)  # To find the length of the data to be hidden
+    data_len = len(binary_secret_msg)
     for values in image:
         for pixel in values:
-            r, g, b = messageToBinary(pixel)  # converts RGB values to binary format
+            r, g, b = messageToBinary(pixel)
             if data_index < data_len:
-                # hide the data into LSB of red pixel
                 pixel[0] = int(r[:-1] + binary_secret_msg[data_index], 2)
                 data_index += 1
             if data_index < data_len:
-                # hide the data into LSB of green pixel
                 pixel[1] = int(g[:-1] + binary_secret_msg[data_index], 2)
                 data_index += 1
             if data_index < data_len:
-                # hide the data into LSB of blue pixel
                 pixel[2] = int(b[:-1] + binary_secret_msg[data_index], 2)
                 data_index += 1
             if data_index >= data_len:
@@ -101,7 +87,6 @@ def hideData(image, secret_message):
     return image
 
 
-# FUNCTION DEFINITION TO DECODE THE HIDDEN MESSAGE FROM STEGO IMAGE
 def showData(image):
     binary_data = ""
     for values in image:
@@ -110,18 +95,15 @@ def showData(image):
             binary_data += r[-1]
             binary_data += g[-1]
             binary_data += b[-1]
-    all_bytes = [binary_data[i:i + 8] for i in range(0, len(binary_data), 8)]  # splitting by 8 bits
-    # conversion from bits to characters
+    all_bytes = [binary_data[i:i + 8] for i in range(0, len(binary_data), 8)]
     decoded_data = ""
     for byte in all_bytes:
         decoded_data += chr(int(byte, 2))
-        if decoded_data[-5:] == "#####":  # checks if we have reached the delimiter
+        if decoded_data[-5:] == "#####":
             break
-    return decoded_data[:-5]  # removes the delimiter to show original hidden msg
+    return decoded_data[:-5]
 
 
-# FUNCTION DEFINITION THAT ACCEPTS INPUT IMAGE NAME AND MESSAGE TO BE HIDDEN FROM USER
-# encode data into image
 def encode_text(encrytedData):
     image_name = input("Enter image name with the extension:")
     image = cv2.imread(image_name)
@@ -133,9 +115,7 @@ def encode_text(encrytedData):
     cv2.imwrite(filename, encoded_image)
 
 
-# FUNCTION DEFINITION TO DECODE THE DATA IN THE IMAGE
 def decode_text():
-    # read the image that consists of hidden image
     image_name = input("Enter the name of steganographed image with the extension:")
     image = cv2.imread(image_name)
     text = showData(image)
